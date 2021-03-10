6994dcdc246ac943af38d2d5a0357c4eeae61f43
moritzsalla <moritz.salla@hotmail.de>
2021-03-10 21:35:09 +0100

    (Hopefully) resolved Parcel bug

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './styles.css';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import decorder from 'three/examples/js/libs/draco/draco_decoder';
-import house from './assets/srt10.glb';
+import house from 'url:./assets/srt10.glb';
 
 let particleCount = 3000;
 let particleSpread = 1000;
@@ -72,7 +72,7 @@ function init() {
     },
     // called while loading is progressing
     function (xhr) {
-      console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+      console.log(Math.round((xhr.loaded / xhr.total) * 100) + '% loaded');
     },
     // called when loading has errors
     function (error) {
